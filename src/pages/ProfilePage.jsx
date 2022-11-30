e9865b8c325dc1c32326ce561c823887643602e9
xko0 <germain.rgb@hotmail.fr>
2022-11-30 09:54:17 +0100

    all import invaled/unused removed

--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -1,4 +1,4 @@
-import React, {useEffect} from 'react'
+import React from 'react'
 import {useNavigate} from 'react-router-dom'
 import { observer } from 'mobx-react-lite'
 import {useState} from 'react'
