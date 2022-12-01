33725112324c7781f5ce2566972f44595aa2e8f1
Chreees79 <christelle.gevaert32@gmail.com>
2022-12-01 21:36:30 +0100

    fix link test API

--- spec/apiSpec.js
+++ spec/apiSpec.js
@@ -2,21 +2,21 @@ import axios from 'axios'
 
 describe('api exists', () => {
   it('GET /info for users should return 200 response', (done) => {
-    axios.get("https://dev-marketplace-api.fly.dev/members")
+    axios.get("https://dev-marketplace-api-immo.fly.dev/members")
      .then((response) => {
        expect(response.status).toBe(200); 
        done();
      })
   })  
   it('GET /info for properties should return 200 response', (done) => {
-    axios.get("https://dev-marketplace-api.fly.dev/properties")
+    axios.get("https://dev-marketplace-api-immo.fly.dev/properties")
      .then((response) => {
        expect(response.status).toBe(200); 
        done();
      })
   })
   it("API Response for members should be valid json", (done) => {
-    axios.get("https://dev-marketplace-api.fly.dev/members")
+    axios.get("https://dev-marketplace-api-immo.fly.dev/members")
       .then((response) => {
         expect(() => {
           JSON.stringify(response.body);
@@ -25,7 +25,7 @@ describe('api exists', () => {
       })
     });  
   it("API Response for properties should be valid json", (done) => {
-  axios.get("https://dev-marketplace-api.fly.dev/properties")
+  axios.get("https://dev-marketplace-api-immo.fly.dev/properties")
     .then((response) => {
       expect(() => {
         JSON.stringify(response.body);
